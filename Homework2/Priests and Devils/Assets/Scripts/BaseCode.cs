60d423f234b7342db157c85bae710ae82aa0d468
chaunceyzhangx <1401046908@qq.com>
2018-04-02 22:04:56 +0800

    update

---
+++ Homework2/Priests and Devils/Assets/Scripts/BaseCode.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Com.MyGame;
+
+namespace Com.MyGame
+{
+    public enum State { LEFT = 0, LEFT_RIGHT, RIGHT_LEFT, RIGHT, WIN, LOSE };//游戏状态
+    public interface Interfaces
+    {
+        void priestLeftOn();//牧师从左边上船
+        void priestRightOn();//牧师从右边上船
+        void devilLeftOn();//魔鬼从左边上船
+        void devilRightOn();//魔鬼从右边上船
+        void moveBoat();//移动船
+        void getOffBoat();//下船
+    }
+
+    public class Director : System.Object,Interfaces
+    {
+
+        private static Director _instance;//单例模式
+        private BaseCode _base;
+        private GenGameObject genGameobj;
+        public State state = State.LEFT;
+
+        public static Director getInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Director();
+            }
+            return _instance;
+        }
+        internal void setBaseCode(BaseCode b)
+        {
+            if (_base == null)
+            {
+                _base = b;
+            }
+        }
+        internal void setGenGameObject(GenGameObject obj)
+        {
+            if (genGameobj == null)
+            {
+                genGameobj = obj;
+            }
+        }
+        public void priestLeftOn()
+        {
+            genGameobj.priestLeftOn();
+        }
+        public void priestRightOn()
+        {
+            genGameobj.priestRightOn();
+        }
+        public void devilLeftOn()
+        {
+            genGameobj.devilLeftOn();
+        }
+        public void devilRightOn()
+        {
+            genGameobj.devilRightOn();
+        }
+        public void moveBoat()
+        {
+            genGameobj.moveBoat();
+        }
+        public void getOffBoat()
+        {
+            genGameobj.getOffBoat();
+        }
+    }
+}
+
+public class BaseCode : MonoBehaviour
+{
+    void Awake()
+    {
+        Director my = Director.getInstance();
+        my.setBaseCode(this);
+    }
+}
---
+++ Homework2/Priests and Devils/Assets/Scripts/UI.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Com.MyGame;
+
+public class UI : MonoBehaviour {
+
+    Director dir;
+    Interfaces userInterface;
+    private float timer = 0f;
+    private int flag = 0;//判断游戏是否结束
+    private float second = 0f;
+    private float minute = 0f;
+    private string str;
+
+
+    void Awake()
+    {
+        dir = Director.getInstance();
+        userInterface = Director.getInstance() as Interfaces;
+    }
+    void Update()
+    {
+        if(flag == 0)
+        {
+            timer += Time.deltaTime;
+            if (timer >= 1f)
+            {
+                second++;
+                timer = 0;
+            }
+            if (second >= 60)
+            {
+                minute++;
+                second = 0;
+            }
+            if (minute >= 60)
+            {
+                minute = 0;
+            }
+        }
+        
+
+    }
+
+    void OnGUI()
+    {
+        str = string.Format("{0:00}:{1:00}", minute, second);//计时器
+        GUIStyle style = new GUIStyle();
+        style.fontSize = 20;
+        GUI.Label(new Rect(0, 0, 100, 200), str, style);
+
+        if (dir.state == State.WIN)
+        {
+            flag = 1;
+            GUIStyle word = new GUIStyle();
+            word.normal.textColor = new Color(0, 0, 1);//设置字体颜色 
+            word.fontSize = 35;//字体大小
+            GUI.TextField(new Rect(290, 20, 80, 50), "WIN", word);
+            Debug.Log("Win");
+        }
+        else if(dir.state == State.LOSE)
+        {
+            flag = 1;
+            GUIStyle word = new GUIStyle();
+            word.normal.textColor = new Color(1, 0, 0);
+            word.fontSize = 35;
+            GUI.TextField(new Rect(290, 20, 80, 50), "LOSE", word);
+            Debug.Log("Lose");
+        }
+        else if(dir.state == State.LEFT||dir.state == State.RIGHT)//其他状态下不能点击，例如移动过程中
+        {    
+            if (GUI.Button(new Rect(470, 100, 80, 50), "PriestLOn"))
+            {
+                userInterface.priestLeftOn();
+            }
+            if (GUI.Button(new Rect(555, 100, 80, 50), "PriestROn"))
+            {
+                userInterface.priestRightOn();
+            }
+            if (GUI.Button(new Rect(470, 150, 80, 50), "DevilLOn"))
+            {
+                userInterface.devilLeftOn();
+            }
+            if (GUI.Button(new Rect(555, 150, 80, 50), "DevilROn"))
+            {
+                userInterface.devilRightOn();
+            }
+            if (GUI.Button(new Rect(470, 200, 80, 50), "GetOff"))
+            {
+                userInterface.getOffBoat();
+            }
+            if (GUI.Button(new Rect(555, 200, 80, 50), "MOVE"))
+            {
+                userInterface.moveBoat();
+            }
+        }
+    }
+}
