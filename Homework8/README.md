f27a47ae0ffce79c41e5a4db3638c45cfbf164cc
chaunceyzhangx <1401046908@qq.com>
2018-06-05 16:48:34 +0800

    IMGUI和UGUI血条制作

---
+++ Homework8/README.md
@@ -0,0 +1,5 @@
+## README
+
+#### 演示视频：[戳这里](http://www.iqiyi.com/w_19rzbljlll.html)
+
+#### 制作流程及分析见博客：[Duke's Blog]()
---
+++ Homework8/血条制作/Assets/Scripts/Attack.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Attack : MonoBehaviour {
+    public Slider slider;
+    // Use this for initialization
+    void Start()
+    {
+        Button btn = this.GetComponent<Button>();
+        btn.onClick.AddListener(Click);
+    }
+    public void Click()
+    {
+        if(slider.value >= 10)
+        {
+            slider.value -= 10;
+        }
+        else
+        {
+            slider.value = 0;
+        }
+    }
+}
